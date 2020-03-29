6e29d46ea4d4502bc7fd2395a24b0633d7e02e2e
Leon Pahole <leonnieen@gmail.com>
2020-03-29 23:35:39 +0200

    Remove dotenv from config

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,11 +1,4 @@
-import dotenv from 'dotenv';
-
 process.env.NODE_ENV = process.env.NODE_ENV || 'development';
-
-const envFound = dotenv.config();
-if (!envFound) {
-  throw new Error('.env file not found'!);
-}
 
 export default {
   port: parseInt(process.env.APP_PORT || '4000', 10),
